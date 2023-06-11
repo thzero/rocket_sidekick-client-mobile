d06c24845eca3918d6efcee6b96b589a1b779f16
thzero <thzero@users.noreply.github.com>
2023-06-11 08:19:32 -0500

    force portrait mode

--- android/app/src/main/java/com/rocketsidekick/MainActivity.java
+++ android/app/src/main/java/com/rocketsidekick/MainActivity.java
@@ -1,5 +1,13 @@
 package com.rocketsidekick;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    protected void onCreate (Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+    }
+}
+
