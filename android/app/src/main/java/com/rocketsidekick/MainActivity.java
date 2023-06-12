c5c34068bd5a082324623f21eeebb3773edb1ee7
thzero <thzero@users.noreply.github.com>
2023-06-12 06:43:53 -0500

    dependency updates

--- android/app/src/main/java/com/rocketsidekick/MainActivity.java
+++ android/app/src/main/java/com/rocketsidekick/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
-
 public class MainActivity extends BridgeActivity {
     protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
     }
 }
